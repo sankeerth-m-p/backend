1cc47ca4a2b0c10f6fe5702d11ecc71630d8e194
sankeerth-m-p <sankeerthajith3@gmail.com>
2026-02-24 10:33:17 +0530

    REFERSH TOKEN SET ADDEE DTOKEN EXPIRY 7 DAYS

--- auth_routes.py
+++ auth_routes.py
@@ -1,9 +1,10 @@
 from flask import Blueprint, request, jsonify
 from werkzeug.security import check_password_hash, generate_password_hash
 from flask_jwt_extended import create_access_token
+from flask_jwt_extended import create_access_token, create_refresh_token
 from models import User
 from extensions import db
-
+rom flask_jwt_extended import jwt_required, get_jwt_identity
 ADMIN_PASSWORD = "password123" 
 auth_bp = Blueprint("auth", __name__)
 @auth_bp.route("/login", methods=["POST"])
@@ -16,9 +17,11 @@ def login():
         return jsonify({"error": "Invalid credentials"}), 401
 
     token = create_access_token(identity=str(user.id)) 
-
+    access_token = create_access_token(identity=str(user.id))
+    refresh_token = create_refresh_token(identity=str(user.id))
     return jsonify({
         "token": token,
+        "refresh_token": refresh_token,
         "username": user.username
     })
 @auth_bp.route("/register", methods=["POST"])
@@ -54,3 +57,9 @@ def register():
         "message": "User created successfully",
         "username": username
     }), 201
+@auth_bp.route("/refresh", methods=["POST"])
+@jwt_required(refresh=True)
+def refresh():
+    user_id = get_jwt_identity()
+    new_access_token = create_access_token(identity=user_id)
+    return jsonify({ "token": new_access_token })
